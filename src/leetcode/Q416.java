7cdf40d31ecaa7e7e78538cbee793be19d92faed
unknown <wjj1252600254@163.com>
2019-04-26 16:00:01 +0800

    426

--- src/leetcode/Q416.java
+++ src/leetcode/Q416.java
@@ -1,6 +1,12 @@
 package leetcode;
 
+import java.util.Arrays;
+
 public class Q416 {
+    /**
+     * @param nums
+     * @return空间优化版本
+     */
     public boolean canPartition(int[] nums) {
         int s = sum(nums);
         if (s % 2 != 0)
@@ -15,6 +21,39 @@ public class Q416 {
             }
         }
         return dp[len];
+    }
+
+    /**
+     * 二维数组
+     * 0   1   2   3   4   5   6   7   8   9   10  11
+     * 0  T   F   F   F   F   F   F   F   F   F   F   F
+     * 1  F   T   F   F   F   F   F   F   F   F   F   F
+     * 2  F   T   F   F   F   F   T   F   F   F   F   F
+     * 3  F   T   F   F   F   F   T   F   F   F   F   T
+     * 4  F   T   F   F   F   F   F   F   F   F   F   T
+     *
+     * @return
+     */
+    public boolean twoArray(int[] nums) {
+        int s = sum(nums);
+        if (s % 2 != 0)
+            return false;
+        int sum = s / 2;
+        int len = nums.length;
+        boolean[][] dps = new boolean[len + 1][sum + 1];
+        dps[0][0] = true;
+        for (int i = 1; i <= len; i++) {
+            int n = nums[i - 1];
+            for (int j = 1; j <= sum; j++) {
+                if (j >= n) {
+                    dps[i][j] = dps[i - 1][j] || dps[i - 1][j - n];
+                } else {
+                    dps[i][j] = dps[i - 1][j];
+                }
+            }
+        }
+        System.out.println(Arrays.deepToString(dps));
+        return dps[len][sum];
     }
 
 /*
